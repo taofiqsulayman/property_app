ef438b18064026b0ab682f9436f8b07ac2465088
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2023-06-25 14:21:49 +0100

    connect to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_25_110206) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "properties", force: :cascade do |t|
     t.string "property_address"
     t.string "property_type"
